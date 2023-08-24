db0f7daeab46b14d8d066e3c59b4a1d3e01756e4
AttivilliPavanKumar <119720777+AttivilliPavanKumar@users.noreply.github.com>
2023-08-24 14:33:04 +0530

    ch=input() if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):     print("VOWEL") elif(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):     print("VOWEL") else:     print("CONSONANT")

---
+++ VOWEL_or_CONSONANT.py
@@ -0,0 +1,7 @@
+ch=input()
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+    print("VOWEL")
+elif(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print("VOWEL")
+else:
+    print("CONSONANT")
